735581ca616db2b2b8a4c3c89694306cdc59ce4f
yasinbudi12 <73303503+yasinbudi12@users.noreply.github.com>
2021-02-08 21:48:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
                 <div class="row">
                     <div class="col-md-8">
                         <h1>coba </h1>
-                        <p>Bergabunglah bersama jutaan orang lainnya...</p>
+                        <p>Bergabung lahhhh ...</p>
                     </div>
                     <div class="col-md-4">
                         <a href="login.php" class="btn btn-secondary">Masuk</a>
