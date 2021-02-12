a8052e830dfd411083cf9477200921a4133e9083
yasinbudi12 <73303503+yasinbudi12@users.noreply.github.com>
2021-02-12 21:06:52 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "devopscilsy";
-$db_pass = "1234567890";
+$db_host = getenv(“DB_HOST");
+$db_user = getenv(“DB_USER”);
+$db_pass = getenv(“DB_PASS”);
 $db_name = "dbsosmed";
 
 try {    
